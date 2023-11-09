7069a0896902afab805957ab3ea5927d69dafa81
tajbaba999 <tajbaba153@gmail.com>
2023-11-10 02:17:03 +0530

    statless auth with jwt

--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,32 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const secret = "TAJ_BABA@123";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  // setUser(id, User)
+  //   sessionIdToUserMap.set(id, user);
+  //   const payload = {
+  // id
+  //     ...user,
+  //   };
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  //   return sessionIdToUserMap.get(id);
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
