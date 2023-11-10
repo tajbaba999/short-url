0bcae59605300292708f68678b07660a4d71b929
tajbaba999 <tajbaba153@gmail.com>
2023-11-10 23:27:48 +0530

    upto complete authtication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,12 @@
 const { getUser } = require("../services/auth");
 
 async function restrictToLoggedUserOnly(req, res, next) {
-  const userid = req.cookie?.uuid;
+  // const userid = req.cookie?.uuid;
+  const userid = req.headers["Authorization"];
 
   if (!userid) return res.redirect("/login");
-  const user = getUser(userid);
+  const token = userid.split("Bearer ")[1];
+  const user = getUser(token);
 
   if (!user) return res.redirect("/login");
 
@@ -13,12 +15,16 @@ async function restrictToLoggedUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-  const userid = req.cookie?.uuid;
-  const user = getUser(userid);
+  // const userid = req.cookie?.uuid;
+  const userid = req.headers["authorization"];
+  const token = userid.split("Bearer ")[1];
+
+  const user = getUser(token);
   req.user = user;
   next();
 }
 
 module.exports = {
   restrictToLoggedUserOnly,
+  checkAuth,
 };
